dbb350a166e766479e970568b5a5449f2364fe46
pankajmishra743 <36882835+pankajmishra743@users.noreply.github.com>
2020-07-15 15:21:50 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+
+#flask_app.py
+import subprocess
+import atexit
+from flask import render_template, render_template_string, Flask
+from bokeh.embed import autoload_server
+from bokeh.client import pull_session
+
+app_html="""
+<!DOCTYPE html>
+<html lang="en">
+  <body>
+    <div class="bk-root">
+      {{ bokeh_script|safe }}
+    </div>
+  </body>
+</html>
+"""
+
+app = Flask(__name__)
+
+bokeh_process = subprocess.Popen(["bokeh", "serve", "--allow-websocket-origin=testbokeh123.herokuapp","--port=$PORT", "--host=testbokeh123.herokuapp.com", "--host=*", "--address=0.0.0.0", "--use-xheaders", "bokeh_plot.py"],stdout=subprocess.PIPE)
+
+@atexit.register
+def kill_server():
+    bokeh_process.kill()
+
+@app.route('/')
+def index():
+    session=pull_session(app_path='/bokeh_plot')
+    bokeh_script=autoload_server(None,app_path="/bokeh_plot",session_id=session.id)
+    # return render_template('app.html', bokeh_script=bokeh_script)
+    return render_template_string(app_html, bokeh_script=bokeh_script)
+
+if __name__ == '__main__':
+    print("STARTED")
+    app.run(debug=True)
