ac3364144c47ae32e06f604820b4033a1124eb57
pankajmishra743 <36882835+pankajmishra743@users.noreply.github.com>
2020-07-16 10:58:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ app_html="""
 
 app = Flask(__name__)
 
-bokeh_process = subprocess.Popen(["bokeh", "serve", "--allow-websocket-origin=testbokeh123.herokuapp.com", "bokeh_plot.py"],stdout=subprocess.PIPE)
+bokeh_process = subprocess.Popen(["bokeh", "serve", "--allow-websocket-origin=http://localhost:5006/", "bokeh_plot.py"],stdout=subprocess.PIPE)
 
 @atexit.register
 def kill_server():
@@ -27,7 +27,7 @@ def kill_server():
 
 @app.route('/')
 def index():
-    session=pull_session(url='https://testbokeh123.herokuapp.com/')
+    session=pull_session(url=url='http://localhost:5006/')
     bokeh_script=server_document(None,app_path="/bokeh_plot",session_id=session.id)
     # return render_template('app.html', bokeh_script=bokeh_script)
     return render_template_string(app_html, bokeh_script=bokeh_script)
