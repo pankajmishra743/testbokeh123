9e98de95807365d4cf01046b6c96cfe42aa6dfbb
pankajmishra743 <36882835+pankajmishra743@users.noreply.github.com>
2020-07-19 00:38:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,9 @@ app_html="""
 
 app = Flask(__name__)
 
-bokeh_process = subprocess.Popen(["bokeh", "serve", "--allow-websocket-origin=http://localhost:5006/", "bokeh_plot.py"],stdout=subprocess.PIPE)
+# bokeh_process = subprocess.Popen(["bokeh", "serve", "--allow-websocket-origin=http://localhost:5006/", "bokeh_plot.py"],stdout=subprocess.PIPE)
+
+bokeh_process = subprocess.Popen(['python', '-m', 'bokeh', 'serve', '--allow-websocket-origin=https://testbokeh123.herokuapp.com/', 'bokeh_plot.py'], stdout=subprocess.PIPE)
 
 @atexit.register
 def kill_server():
@@ -27,8 +29,9 @@ def kill_server():
 
 @app.route('/')
 def index():
-    session=pull_session(url='http://localhost:5006/')
-    bokeh_script=server_document(None,app_path="/bokeh_plot",session_id=session.id)
+    #session=pull_session(url='http://localhost:5006/')
+    #bokeh_script=server_document(None,app_path="/bokeh_plot",session_id=session.id)
+    bokeh_script = server_document(url='https://testbokeh123.herokuapp.com/bokeh_plot', arguments=dict(batchid=batchid))
     # return render_template('app.html', bokeh_script=bokeh_script)
     return render_template_string(app_html, bokeh_script=bokeh_script)
 
